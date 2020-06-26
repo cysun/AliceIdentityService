8ac5d54bf07818203eaf15ffd6e9fe5563d4b97f
Chengyu Sun <csun@calstatela.edu>
2020-06-25 20:00:07 -0700

    Added a ConsoleDbManager project

---
+++ ConsoleDbManager/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using AliceIdentityService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleDbManager
+{
+    class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private static readonly string ConnectionString;
+
+        static AppDbContext()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../AliceIdentityService"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+            ConnectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+    }
+}
---
+++ ConsoleDbManager/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleDbManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new AppDbContext();
+            var users = db.Users.Select(u => u);
+            foreach (var user in users)
+                Console.WriteLine(user);
+        }
+    }
+}
