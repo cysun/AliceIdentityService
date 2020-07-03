b4fff74a48e1c0082e4afaee7a8334abfd425462
Chengyu Sun <csun@calstatela.edu>
2020-07-03 11:50:18 -0700

    Replaced ConsoleDbManager with ConsoleManager
    Using services via Dependency Injection instead of using DbContext directly.

---
+++ ConsoleManager/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AliceIdentityService.Models;
+using AliceIdentityService.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ConsoleManager
+{
+    partial class ConsoleManager
+    {
+        private readonly ServiceProvider serviceProvider;
+
+        public UserManager<ApplicationUser> UserManager => serviceProvider.GetService<UserManager<ApplicationUser>>();
+
+        public ConsoleManager()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../AliceIdentityService"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<ApplicationUser>().AddEntityFrameworkStores<AppDbContext>();
+            serviceProvider = services.BuildServiceProvider();
+
+            var userManager = serviceProvider.GetService<UserManager<ApplicationUser>>();
+            foreach (var user in userManager.Users)
+                Console.WriteLine(user.UserName);
+        }
+
+        public void MainController()
+        {
+            var done = false;
+            do
+            {
+                var cmd = MainView();
+                switch (cmd)
+                {
+                    case "u":
+                        UsersController();
+                        break;
+                    case "x":
+                        done = true;
+                        break;
+                }
+            } while (!done);
+        }
+
+        public string MainView()
+        {
+            var validChoices = new HashSet<string>() { "u", "x" };
+            string choice;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\t Main Menu \n");
+                Console.WriteLine("\t u) User Management");
+                Console.WriteLine("\t x) Exit");
+                Console.Write("\n  Pleasse enter your choice: ");
+                choice = Console.ReadLine().ToLower();
+            } while (!validChoices.Contains(choice));
+
+            return choice;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new ConsoleManager()).MainController();
+        }
+    }
+}
---
+++ ConsoleManager/UserManagement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliceIdentityService.Models;
+
+namespace ConsoleManager
+{
+    partial class ConsoleManager
+    {
+        public void UsersController()
+        {
+            var done = false;
+            do
+            {
+                var users = UserManager.Users.OrderBy(u => u.UserName).ToList();
+                var cmd = UsersView(users);
+                if (cmd == "m")
+                {
+                    done = true;
+                }
+                else
+                {
+                    int index;
+                    bool isNumber = int.TryParse(cmd, out index);
+                    if (isNumber && index < users.Count)
+                        UserView(users[index]);
+                }
+            } while (!done);
+        }
+
+        public string UsersView(List<ApplicationUser> users)
+        {
+            var validChoices = new HashSet<string>() { "m" };
+            for (int i = 0; i < users.Count; ++i)
+                validChoices.Add(i.ToString());
+
+            string choice;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"\t User Management \n");
+                Console.WriteLine("\t m) Back to Main Menu");
+                for (int i = 0; i < users.Count; ++i)
+                    Console.WriteLine($"\t {i}) {users[i].UserName}");
+                Console.Write("\n Pleasse enter your choice: ");
+                choice = Console.ReadLine().ToLower();
+            } while (!validChoices.Contains(choice));
+            return choice;
+        }
+
+        public void UserView(ApplicationUser user)
+        {
+            Console.Clear();
+            Console.WriteLine($"\t User Management - {user.UserName} \n");
+            Console.Write("\n Press [Enter] key to go back");
+            Console.ReadLine().ToLower();
+        }
+    }
+}
